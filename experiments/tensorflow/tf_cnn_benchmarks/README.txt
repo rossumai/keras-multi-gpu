e344ffedad2f4957c995bbe61d0196e8bcd56304
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2017-09-29 15:45:08 +0200

    Update the benchmark runner script to use existing installation of tensorflow-benchmark.

--- experiments/tensorflow/tf_cnn_benchmarks/README.txt
+++ experiments/tensorflow/tf_cnn_benchmarks/README.txt
@@ -1,1 +1,10 @@
+```
+git clone https://github.com/tensorflow/benchmarks tensorflow-benchmarks
+export TF_BENCHMARKS_PATH=tensorflow-benchmarks/scripts/tf_cnn_benchmarks
+export MACHINE=az2xm60
+CUDA_VISIBLE_DEVICES=0,1 time ./benchmark_batch.sh $TF_BENCHMARKS_PATH 2>&1 |tee benchmark_batch_results_$MACHINE_$(date +%Y-%m-%d).txt
+```
+
 Suite run time: 13m
+
+https://docs.google.com/spreadsheets/d/1c5yGydEANMzHjBufTzph0w-WGwJyiwPMRYz3yBZatb4/edit?usp=sharing
--- experiments/tensorflow/tf_cnn_benchmarks/benchmark_batch.sh
+++ experiments/tensorflow/tf_cnn_benchmarks/benchmark_batch.sh
@@ -1,6 +1,8 @@
 # run as:
-# time ./benchmark_batch.sh 2>&1 |tee benchmark_batch_results.txt
+# time ./benchmark_batch.sh tensorflow-benchmarks/scripts/tf_cnn_benchmarks 2>&1 |tee benchmark_batch_results.txt
 set -x
+
+TF_BENCHMARKS_PATH=$1
 
 # models: alexnet, densenet, googlenet, inception, lenet, overfeat, resnet, trivial, vgg
 # models that don't need imagenet dataset: alexnet, googlenet, lenet, overfeat, trivial
@@ -10,15 +12,15 @@ set -x
 # --variable_update parameter_server
 # --use_nccl True (only for --variable_update replicated)
 # --data_format NCHW
-# compare these to: python tf_cnn_benchmarks.py --model alexnet --num_gpus 2
-python tf_cnn_benchmarks.py --model alexnet --num_gpus 2 --local_parameter_device cpu
-python tf_cnn_benchmarks.py --model alexnet --num_gpus 2 --variable_update replicated
-python tf_cnn_benchmarks.py --model alexnet --num_gpus 2 --variable_update replicated --use_nccl False
-python tf_cnn_benchmarks.py --model alexnet --num_gpus 2 --variable_update independent
-python tf_cnn_benchmarks.py --model alexnet --num_gpus 2 --data_format NHWC
+# compare these to: python ${TF_BENCHMARKS_PATH}/tf_cnn_benchmarks.py --model alexnet --num_gpus 2
+python ${TF_BENCHMARKS_PATH}/tf_cnn_benchmarks.py --model alexnet --num_gpus 2 --local_parameter_device cpu
+python ${TF_BENCHMARKS_PATH}/tf_cnn_benchmarks.py --model alexnet --num_gpus 2 --variable_update replicated
+python ${TF_BENCHMARKS_PATH}/tf_cnn_benchmarks.py --model alexnet --num_gpus 2 --variable_update replicated --use_nccl False
+python ${TF_BENCHMARKS_PATH}/tf_cnn_benchmarks.py --model alexnet --num_gpus 2 --variable_update independent
+python ${TF_BENCHMARKS_PATH}/tf_cnn_benchmarks.py --model alexnet --num_gpus 2 --data_format NHWC
 
 for model in trivial alexnet lenet googlenet overfeat; do
   for num_gpus in 1 2; do
-    python tf_cnn_benchmarks.py --model $model --num_gpus $num_gpus
+    python ${TF_BENCHMARKS_PATH}/tf_cnn_benchmarks.py --model $model --num_gpus $num_gpus
   done
 done
