cd0616e40a03030889050ae7a97d24d000bf93c9
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2017-10-02 14:07:02 +0200

    Modify the example from kuza55 - configure gpus, epochs, ps_device via CLI args.

--- experiments/keras_tensorflow/kuza55/original_examples/kuza55_blog_example.py
+++ experiments/keras_tensorflow/kuza55/original_examples/kuza55_blog_example.py
@@ -1,14 +1,19 @@
 # Multi-GPU Keras training.
 # Captured from https://github.com/kuza55/keras-extras and
 # https://medium.com/@kuza55/transparent-multi-gpu-training-on-tensorflow-with-keras-8b0016fd9012.
+#
+# Adapted for easier measurement: arg parser, etc.
 
-from keras.layers import merge
+import argparse
+import numpy as np
+import keras
+from keras.layers import merge, Dense
 from keras.layers.core import Lambda
-from keras.models import Model
-
+from keras.models import Model, Sequential
 import tensorflow as tf
 
-def make_parallel(model, gpu_count):
+# source: https://github.com/kuza55/keras-extras/blob/master/utils/multi_gpu.py
+def make_parallel(model, gpu_count, ps_device):
     def get_slice(data, idx, parts):
         shape = tf.shape(data)
         size = tf.concat([ shape[:1] // parts, shape[1:] ],axis=0)
@@ -42,27 +47,36 @@ def make_parallel(model, gpu_count):
                     outputs_all[l].append(outputs[l])
 
     # merge outputs on CPU
-    with tf.device('/cpu:0'):
+    with tf.device(ps_device):
         merged = []
         for outputs in outputs_all:
             merged.append(merge(outputs, mode='concat', concat_axis=0))
 
         return Model(input=model.inputs, output=merged)
 
-import numpy as np
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
-model = Sequential()
-model.add(Dense(4000, input_dim=8000, activation='tanh'))
-model.add(Dense(2000, input_dim=8000, activation='relu'))
-model.add(Dense(500, activation='relu'))
-model.add(Dense(300, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-print (model.summary())
-model = make_parallel(model, 4)
-optimizer = keras.optimizers.Adam(lr=0.0001)
-model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-x = np.random.rand(131072, 8000)
-y = np.random.randint(0, 2, (131072, 1))
-model.fit(x, y, batch_size=2048*4)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='kuza55 make_parallel test.')
+    parser.add_argument('-g', '--gpus', default=1, type=int, help='Number of GPUs to use')
+    parser.add_argument('-p', '--parameter-server', default='cpu', help='Parameter server device (cpu, gpu)')
+    parser.add_argument('-e', '--epochs', default=5, type=int, help='Number of epochs')
+    args = parser.parse_args()
+    ps_device = '/%s:0' % args.parameter_server
+
+    # source: https://medium.com/@kuza55/transparent-multi-gpu-training-on-tensorflow-with-keras-8b0016fd9012
+    with tf.device(ps_device):
+        model = Sequential()
+        model.add(Dense(4000, input_dim=8000, activation='tanh'))
+        model.add(Dense(2000, input_dim=8000, activation='relu'))
+        model.add(Dense(500, activation='relu'))
+        model.add(Dense(300, activation='relu'))
+        model.add(Dense(1, activation='sigmoid'))
+        print (model.summary())
+        if args.gpus > 1:
+            model = make_parallel(model, args.gpus, ps_device)
+        optimizer = keras.optimizers.Adam(lr=0.0001)
+        model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+    x = np.random.rand(131072, 8000)
+    y = np.random.randint(0, 2, (131072, 1))
+
+    model.fit(x, y, batch_size=2048*args.gpus, epochs=args.epochs)
